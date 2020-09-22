4beb3597dc05bcfccad993b13dfac85815df7800
SAN-LUIS <victormbarriosb@gmail.com>
2020-09-22 12:20:53 -0400

    First

---
+++ DataBase.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="VMBB",
+  password="vlocmalhbostb",
+  database="lkstock"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT Descripcion FROM lkstock.productos LIMIT 10")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
